18b7c49b5249281260f321953aeee7cf8a12d91b
JD <jonny-d1994@hotmail.com>
2025-08-03 16:18:21 +0100

    update

--- networking/infrastructure/terraform/main.tf
+++ networking/infrastructure/terraform/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source  = "Azure/avm-res-resources-resourcegroup/azurerm"
   version = "0.2.1"
-  name = "rg-${var.environment}-${var.location}-01"
+  name = "rg-${var.environment}-${var.location}-001"
   location = var.location
 }
